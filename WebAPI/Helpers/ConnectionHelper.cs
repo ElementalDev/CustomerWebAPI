62e432540243e5623d0cf517695da81d08e96f2d
Reece <reece.collett@sra.org.uk>
2020-09-16 15:16:50 +0100

    Added transforms

---
+++ WebAPI/Helpers/ConnectionHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+
+namespace WebAPI.Helpers
+{
+    public class ConnectionHelper
+    {
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["apiDatabase"].ConnectionString;
+        }
+    }
+}
